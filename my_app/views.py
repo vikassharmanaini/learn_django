a1c99421f7ebe2af5c6ac9a47b89853437cccd0a
Vikassharmanaini <vikassharmanaini@gmail.com>
2022-09-01 20:59:58 +0530

    static and template config

--- my_app/views.py
+++ my_app/views.py
@@ -1,8 +1,12 @@
+from multiprocessing import context
 from django.shortcuts import render,HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request,"index.html")
+    context={
+        'variable':"this is var"
+    }
+    return render(request,"index.html",context)
     # return HttpResponse("<h1>this is home page</h1>")
 def about(request):
     return HttpResponse("About page")
